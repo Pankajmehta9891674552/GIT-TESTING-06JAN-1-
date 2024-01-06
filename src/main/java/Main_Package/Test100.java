ea7f95af6b549a903d65ea7675cfe0f5cf2a8ec7
PANKAJMEHTA <pankaj.mehta989@gmail.com>
2024-01-06 14:19:30 +0530

    CONTROL-M DASHBOARD 06 JAN FIRST COMMIT

---
+++ src/main/java/Main_Package/Test100.java
@@ -0,0 +1,22 @@
+package Main_Package;
+
+import org.testng.annotations.Test;
+
+public class Test100 {
+	
+	
+	@Test
+	public void test100()
+	{
+		System.out.println("First100");
+	}
+	
+	@Test
+	public void test101()
+	{
+		System.out.println("Second101");
+	}
+	
+	
+
+}
---
+++ src/test/java/Test_Package/Test200.java
@@ -0,0 +1,25 @@
+package Test_Package;
+
+import org.testng.annotations.Test;
+
+public class Test200 {
+	
+	
+	@Test
+	public void test200()
+	{
+		System.out.println("First200");
+	}
+	
+	@Test
+	public void test201()
+	{
+		System.out.println("Second201");
+	}
+	
+	
+	
+	
+	
+
+}
